8ac85dacd6be0be38cf94f8b7a337e4905812f51
33 <1554470705@qq.com>
2024-08-19 09:48:04 +0800

    06

--- day04/类和对象/学生信息录入.py
+++ day04/类和对象/学生信息录入.py
@@ -13,5 +13,5 @@ for i in range(1, 11):
     name = input("请输入学生姓名：")
     age = int(input("请输入学生年龄："))
     address = input("请输入学生地址：")
-    stu = student(name, age, address)
+    stu = Student(name, age, address)
     print(f"学生{i}信息录入完成,信息为：【学生姓名；{stu.name},年龄：{stu.age},地址：{stu.address}】")
